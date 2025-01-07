b64db9c360eee111b36a57390677de36b5852515
shashi-chaurasia <shashichaurasia886@gmail.com>
2025-01-07 14:02:10 +0600

    added

--- app.py
+++ app.py
@@ -1,13 +1,39 @@
-import os
-import sys
+from flask import Flask
+from models import db
+from routes import main_routes
+from routes.admin import admin_routes, faculty_routes, course_routes, facilities_routes, count_routes, campus_routes, \
+    notification_routes, news_routes
+from flask_migrate import Migrate
 
+def create_app():
+    app = Flask(__name__)
+    app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///kishanpgcollege.db'
+    app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+    app.secret_key = 'your_secret_key'
+    UPLOAD_FOLDER = 'static/uploads/faculty'
+    app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
 
-sys.path.insert(0, os.path.dirname(__file__))
+    db.init_app(app)
+    Migrate(app, db)
+    # Register blueprints
+    main_routes.register_routes(app)
+    admin_routes.register_routes(app)
+    faculty_routes.register_routes(app)
+    course_routes.register_routes(app)
+    facilities_routes.register_routes(app)
+    count_routes.register_routes(app)
+    campus_routes.register_routes(app)
+    notification_routes.register_routes(app)
+    news_routes.register_routes(app)
 
+    # Initialize the database if running for the first time
+    with app.app_context():
+        db.create_all()
 
-def app(environ, start_response):
-    start_response('200 OK', [('Content-Type', 'text/plain')])
-    message = 'It works!\n'
-    version = 'Python v' + sys.version.split()[0] + '\n'
-    response = '\n'.join([message, version])
-    return [response.encode()]
+    return app
+
+# Expose WSGI callable as "application"
+application = create_app()
+
+if __name__ == "__main__":
+    application.run(port=5005, debug=True)
